c70f8697d0cacba5f9316375153481d17a0e3782
TainaraGATONA <144234661+TainaraGATONA@users.noreply.github.com>
2024-04-26 16:43:15 -0300

    Add files via upload

--- tainara2024/main.js
+++ tainara2024/main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll("aba-conteudo");
 console.log(botoes)
 for (let i=0; i <  botoes.length; i++) {
     console.log(i);
@@ -7,7 +8,8 @@ for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function() {
         for(let j=0;j<botoes.length;j++) { 
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
       }  
         botoes[i].classList.add("ativo");
-    }
-}
+        textos[I].classList.add("ativo")  
+}}
